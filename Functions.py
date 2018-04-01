d17e586de262ae5dc69d274bdc6f5f57ca4179dd
dLenkiewicz <dagmara.lenkiewicz@gmail.com>
2018-04-01 22:09:01 +0200

    Initial commit.

---
+++ Functions.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def generate_data(X, F, B, u, Q, H, v, T):
+    for i in range(T - 1):
+        w = np.random.multivariate_normal(np.zeros(4), Q).T
+        X[i + 1,] = np.matmul(F, X[i,]) + B * u + w
+    X = np.transpose(X)
+    Y = np.matmul(H, X) + v
+    return (X, Y)
+
+
+def kalman_filter(X, Y, F, B, u, Q, H, R, Sigma, T):
+    X_kk = np.zeros((T, 4))
+    Y_d = np.zeros((T, 2))
+    Sigma_kk = np.zeros((T, 4, 4))
+    for i in range(T):
+        Y_d[i,] = Y[i,] - np.matmul(H, X)
+        S = R + np.matmul(np.matmul(H, Sigma), np.transpose(H))
+        K = np.matmul(np.matmul(Sigma, np.transpose(H)), np.linalg.inv(S))
+        X_kk[i,] = X + np.matmul(K, Y_d[i,])
+        Sigma_kk[i, :, :] = Sigma - np.matmul(np.matmul(K, H), Sigma)
+        X = np.matmul(F, X_kk[i,]) + B * u
+        Sigma = np.matmul(np.matmul(F, Sigma_kk[i, :, :]), np.transpose(F)) + Q
+    return (X_kk, Sigma_kk)
---
+++ Results.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from Functions import generate_data, kalman_filter
+
+### Data generator ###
+
+d = 4
+p = 1
+m = 2
+T = 500
+dt = 0.1
+
+F = np.eye((d))
+F[0,2] = dt
+F[1,3] = dt
+
+B = np.array([0,-0.5*dt**2, 0 ,-0.5*dt**2 ])
+
+u=9.8
+
+Q = np.eye((d)) * 0.01
+
+R = np.eye((2)) * 100
+
+X = np.zeros((T, 4))
+X[0, :] = np.array([10 , 1000, 5, 10 ])
+
+v = np.random.multivariate_normal(np.array([0,0]), R, T).T
+
+H = np.zeros((2, 4))
+H[0,0] = 1
+H[1,1] = 1
+
+X, Y = generate_data(X=X, F=F, B=B, u=u, Q=Q, H=H, v=v, T=T)
+
+plt.scatter(X[0, :], X[1, :], s=1, color='crimson')
+plt.scatter(Y[0, :], Y[1, :], s=1, color='blue')
+
+### Kalman filter ###
+
+Y = np.transpose(Y)
+X = np.array([10, 1000, 5, 10]).T
+Sigma = np.zeros((4, 4))
+
+X_kk, Sigma_kk = kalman_filter(X=X, Y=Y, F=F, B=B, u=u, Q=Q, H=H, R=R, Sigma=Sigma, T=T)
+
+plt.scatter(X_kk[1:-1, 0], X_kk[1:-1, 1], s=1, color='green')
+plt.show()
