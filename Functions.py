93c6ec75ec6d36bd921834ffbda7da1cd3fe6ed7
Asia <asiaa93@o2.pl>
2018-04-09 20:28:30 +0200

    cleaning

--- Functions.py
+++ Functions.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def generate_data(X, F, B, u, Q, H, v, T):
+def generate_data(X, F, B, u, Q, H, R, T):
+    v = np.random.multivariate_normal(np.array([0, 0]), R, T).T
     for i in range(T - 1):
         w = np.random.multivariate_normal(np.zeros(4), Q).T
         X[i + 1,] = np.matmul(F, X[i,]) + B * u + w
@@ -9,19 +10,32 @@ def generate_data(X, F, B, u, Q, H, v, T):
     Y = np.matmul(H, X) + v
     return X, Y
 
-def generate_QR_EM(X, Y ,F, H):
-    n, dim_x = X.shape
+def generate_QR_EM(smooth_x,smooth_sig,L_smooth, X_kk, Y ,F, H):
+    n, dim_x = X_kk.shape
     Q_est = np.zeros((dim_x, dim_x))
     R_est = np.zeros((2, 2))
-    for k in range(n-1 , -1, -1):
-        if k>0:
-            Q_est += np.outer(X[k], X[k].T) + np.matmul(np.matmul(F, np.outer(X[k - 1], X[k - 1].T)), F.T) - np.matmul(
-                 np.outer(X[k], X[k - 1].T), F.T) - np.matmul(F, np.outer(X[k - 1], X[k].T))
-        R_est += np.outer(Y[k], Y[k].T) + np.matmul(np.matmul(H, np.outer(X[k], X[k].T)), H.T) - np.matmul(
-            np.outer(Y[k], X[k].T), H.T) - np.matmul(H, np.outer(X[k], Y[k].T))
+
+    #Expectation
+    Xk = smooth_x
+    XkXkT = np.zeros((n, dim_x, dim_x))
+    XkXk1T = np.zeros((n, dim_x, dim_x))
+    for j in range(n - 1, -1, -1):
+        XkXkT[j] = np.outer(smooth_x[j], smooth_x[j].T) + smooth_sig[j]
+        bracket = smooth_sig[j] + np.outer((smooth_x[j] - X_kk[j]), smooth_x[j].T)
+        if j>0:
+            XkXk1T[j-1] = np.outer(X_kk[j - 1], smooth_x[j].T) + np.matmul(L_smooth[j - 1], bracket)
+    #Maximalization
+    for k in range(n - 1, -1, -1):
+        if k > 0:
+            Q_est += XkXkT[k] + np.matmul(np.matmul(F, XkXkT[k - 1]), F.T) - np.matmul(
+                XkXk1T[k - 1].T, F.T) - np.matmul(F, XkXk1T[k - 1])
+        R_est += np.outer(Y[k], Y[k].T) + np.matmul(np.matmul(H, XkXkT[k]), H.T) - np.matmul(
+            np.outer(Y[k], Xk[k].T), H.T) - np.matmul(H, np.outer(Xk[k], Y[k].T))
     Q_est = Q_est / (n - 1)
     R_est = R_est / (n)
     return Q_est, R_est
+
+
 
 def kalman_filter(X, Y, F, B, u, Q, H, R, Sigma, T):
     X_kk = np.zeros((T, 4))
--- Results.py
+++ Results.py
@@ -11,11 +11,10 @@ B = np.array([0,-0.5*dt**2, 0 ,-0.5*dt**2 ])
 u=9.8
 Q = np.eye((d)) * 0.01
 R = np.eye((m)) * 100
-X = np.zeros((T, 4)); X[0, :] = np.array([10 , 1000, 5, 10 ])
-v = np.random.multivariate_normal(np.array([0,0]), R, T).T
+X0=np.array([10 , 1000, 5, 10 ]); X = np.zeros((T, 4)); X[0, :] = X0
 H = np.zeros((m, d)); H[0,0] = 1; H[1,1] = 1
 
-X, Y = generate_data(X=X, F=F, B=B, u=u, Q=Q, H=H, v=v, T=T)
+X, Y = generate_data(X=X, F=F, B=B, u=u, Q=Q, H=H, R=R, T=T)
 plt.scatter(X[0, :], X[1, :], s=1, color='crimson')
 plt.scatter(Y[0, :], Y[1, :], s=1, color='blue')
 X=X.T
@@ -25,44 +24,50 @@ Y=Y.T
 
 ### Kalman filter ###
 
-X = np.array([10, 1000, 5, 10]).T
 Sigma = np.zeros((4, 4))
 
-X_kk, Sigma_kk = kalman_filter(X=X, Y=Y, F=F, B=B, u=u, Q=Q, H=H, R=R, Sigma=Sigma, T=T)
+X_kk, Sigma_kk = kalman_filter(X=X0, Y=Y, F=F, B=B, u=u, Q=Q, H=H, R=R, Sigma=Sigma, T=T)
 
-plt.scatter(X_kk[1:-1, 0], X_kk[1:-1, 1], s=1, color='green')
-plt.show()
+# plt.scatter(X_kk[1:-1, 0], X_kk[1:-1, 1], s=1, color='green')
+# plt.show()
 
 #rts smoothing#
 smooth_x, smooth_sig, L_smooth = rts_smoothing(X_kk, Sigma_kk, F, Q)
 
-plt.scatter(X_kk[1:-1, 0], X_kk[1:-1, 1], s=1, color='green')
-plt.scatter(smooth_x[1:-1, 0], smooth_x[1:-1, 1], s=1, color='black')
-plt.show()
+# plt.scatter(X_kk[1:-1, 0], X_kk[1:-1, 1], s=1, color='green')
+# plt.scatter(smooth_x[1:-1, 0], smooth_x[1:-1, 1], s=1, color='black')
+# plt.show()
 
 
 
 ## Repeat Kalman for Q and R from EM
-
-X = np.zeros((T, 4)); X[0, :] = np.array([10 , 1000, 5, 10 ])
-X, Y = generate_data(X=X, F=F, B=B, u=u, Q=Q, H=H, v=v, T=T)
-X=X.T
-Y=Y.T
-Q_EM, R_EM = generate_QR_EM(X, Y ,F, H)
-
-# data with estimated Q R
-v_EM = np.random.multivariate_normal(np.array([0,0]), R_EM, T).T
-X_EM, Y_EM = generate_data(X=X, F=F, B=B, u=u, Q=Q_EM, H=H, v=v_EM, T=T)
-
-plt.scatter(X_EM[0, :], X_EM[1, :], s=1, color='crimson')
-plt.scatter(Y_EM[0, :], Y_EM[1, :], s=1, color='blue')
-
-Y_EM=Y_EM.T
-X = np.array([10, 1000, 5, 10]).T
-Sigma = np.zeros((4, 4))
-
-X_kk_EM, Sigma_kk_EM = kalman_filter(X=X, Y=Y_EM, F=F, B=B, u=u, Q=Q_EM, H=H, R=R_EM, Sigma=Sigma, T=T)
+Q_init=Q
+R_init=R
+for j in range(200):
+    Q_EM, R_EM = generate_QR_EM(smooth_x,smooth_sig,L_smooth, X_kk, Y ,F, H)
+    Q=Q_EM
+    R=R_EM
+    X_kk, Sigma_kk = kalman_filter(X=X0, Y=Y, F=F, B=B, u=u, Q=Q, H=H, R=R, Sigma=Sigma, T=T)
+    smooth_x, smooth_sig, L_smooth = rts_smoothing(X_kk, Sigma_kk, F, Q)
+print("Qem",Q_EM)
+print("Q", Q_init)
+print("Rem",R_EM)
+print("R", R_init)
 
 
-plt.scatter(X_kk_EM[1:-1, 0], X_kk_EM[1:-1, 1], s=1, color='green')
-plt.show()
+# # data with estimated Q R
+# v_EM = np.random.multivariate_normal(np.array([0,0]), R_EM, T).T
+# X_EM, Y_EM = generate_data(X=X, F=F, B=B, u=u, Q=Q_EM, H=H, v=v_EM, T=T)
+#
+# # plt.scatter(X_EM[0, :], X_EM[1, :], s=1, color='crimson')
+# # plt.scatter(Y_EM[0, :], Y_EM[1, :], s=1, color='blue')
+#
+# Y_EM=Y_EM.T
+# X = np.array([10, 1000, 5, 10]).T
+# Sigma = np.zeros((4, 4))
+#
+# X_kk_EM, Sigma_kk_EM = kalman_filter(X=X, Y=Y_EM, F=F, B=B, u=u, Q=Q_EM, H=H, R=R_EM, Sigma=Sigma, T=T)
+#
+# #
+# # plt.scatter(X_kk_EM[1:-1, 0], X_kk_EM[1:-1, 1], s=1, color='green')
+# # plt.show()
